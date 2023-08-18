749a1465be277e130438830af20ea062c24696fe
Angelica Beltran <angelica.beltran11@gmail.com>
2023-08-18 18:43:23 -0500

    variables are being registered from the inputs

--- script.js
+++ script.js
@@ -8,8 +8,31 @@ $('.submit-button').on('click', getTheInput);
 
 function getTheInput(event){
 
-}//end of get the input function
-
 event.preventDefault();
-console.log("in the getTheInput function");
-  //Takes input and stores it
+console.log("in the getTheInput function");  
+//Takes input and stores it
+//-----------------------------------------------Input Variables-------
+let firstNameInput = $('firstName').val();
+console.log('first name variable is working');
+
+let lastNameInput = $('lastName').val();
+console.log('last name variable is working');
+
+let idInputVar = $('idInput').val();
+console.log('Id variable is working');
+
+let titleInputVar = $('titleInput').val();
+console.log('title variable is working');
+
+let salaryInputVar = $('annualSalaryInput').val();
+console.log('salary variable is working');
+
+}//end of get the input function
+//-----------------------------------------------Input Variables-------
+
+
+
+
+//stop the page from reloading on submit❎
+//the inputs are saved somewhere- in this case a variable...❎
+//grab the inputs
