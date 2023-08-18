f56e2a18d336c7328ea17b0f903ee92f8980da62
Angelica Beltran <angelica.beltran11@gmail.com>
2023-08-18 18:21:22 -0500

    added basic on ready and prevent event default into the script

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 $(document).ready(onReady);
+
+function onReady(){
+     // Submit Button Listener
+$('.submit-button').on('click', getTheInput);
+
+}//end of onREADY
+
+function getTheInput(event){
+
+}//end of get the input function
+
+event.preventDefault();
+console.log("in the getTheInput function");
+  //Takes input and stores it
