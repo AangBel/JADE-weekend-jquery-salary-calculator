6c2924289feeaeca9ce631014b46296d65bb779b
Angelica Beltran <angelica.beltran11@gmail.com>
2023-08-18 19:09:41 -0500

    put values into variables and then used variables to use jquery to manipulate the DOM, they are currently populating in the right spots

--- script.js
+++ script.js
@@ -7,25 +7,45 @@ $('.submit-button').on('click', getTheInput);
 }//end of onREADY
 
 function getTheInput(event){
-
 event.preventDefault();
+
 console.log("in the getTheInput function");  
-//Takes input and stores it
-//-----------------------------------------------Input Variables-------
-let firstNameInput = $('firstName').val();
-console.log('first name variable is working');
 
-let lastNameInput = $('lastName').val();
+let firstNameInput = $('#firstName').val();
+console.log('first name variable is working', $('firstName').val());
+
+let lastNameInput = $('#lastName').val();
 console.log('last name variable is working');
+// $('#tableBody').append(`<td>${lastNameInput}</td>`);
 
-let idInputVar = $('idInput').val();
+let idInputVar = $('#idInput').val();
 console.log('Id variable is working');
 
-let titleInputVar = $('titleInput').val();
+let titleInputVar = $('#titleInput').val();
 console.log('title variable is working');
 
-let salaryInputVar = $('annualSalaryInput').val();
+let salaryInputVar = $('#annualSalaryInput').val();
 console.log('salary variable is working');
+
+//Takes input and stores it
+//-----------------------------------------------Input Variables-------
+
+$('#tableBody').append(`<tr id="tableRow"><td>${firstNameInput}</td><td>${lastNameInput}</td><td>${idInputVar}</td><td>${titleInputVar}</td><td>${salaryInputVar}</td></tr>`);
+
+
+
+// let lastNameInput = $('#lastName').val();
+// console.log('last name variable is working');
+// $('#tableBody').append(`<td>${lastNameInput}</td>`);
+
+// let idInputVar = $('#idInput').val();
+// console.log('Id variable is working');
+
+// let titleInputVar = $('#titleInput').val();
+// console.log('title variable is working');
+
+// let salaryInputVar = $('#annualSalaryInput').val();
+// console.log('salary variable is working');
 
 }//end of get the input function
 //-----------------------------------------------Input Variables-------
@@ -36,3 +56,5 @@ console.log('salary variable is working');
 //stop the page from reloading on submit❎
 //the inputs are saved somewhere- in this case a variable...❎
 //grab the inputs
+//i want the inputs to now appear in the table
+//i do this by tying those new variables to the jquery appending stuff to the html table 
